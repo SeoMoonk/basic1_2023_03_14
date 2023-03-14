a6b047e04df0cc773c3eb51ab35420b0f54f5cd7
SeoMoonk <sunghoon1216@naver.com>
2023-03-14 10:16:37 +0900

    세팅

---
+++ src/main/java/com/ll/basic1/Basic1Application.java
@@ -0,0 +1,17 @@
+package com.ll.basic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basic1Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+
+		SpringApplication.run(Basic1Application.class, args);
+
+	}
+
+}
